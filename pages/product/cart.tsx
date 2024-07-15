bb81a14bfbc5bfd29139c3f350218c35d1585d1a
Bhathiya Prasad <90783913+BhathiyaPrasad@users.noreply.github.com>
2024-07-15 13:48:22 +0530

    added cart

---
+++ pages/product/cart.tsx
@@ -0,0 +1,12 @@
+import MainLayout from '../components/layout/MainLayout';
+import Cart from '../components/Cart';
+
+const CartPage = () => {
+  return (
+    <MainLayout>
+      <Cart />
+    </MainLayout>
+  );
+};
+
+export default CartPage;
