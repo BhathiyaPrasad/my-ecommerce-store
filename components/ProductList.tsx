536a34cd8d8aeeb541383eea6e77aacb86f8c150
Bhathiya Prasad <90783913+BhathiyaPrasad@users.noreply.github.com>
2024-07-15 13:46:04 +0530

    added product list

---
+++ components/ProductList.tsx
@@ -0,0 +1,40 @@
+// components/ProductList.tsx
+import { useEffect, useState } from 'react';
+import { collection, getDocs } from 'firebase/firestore';
+import { db } from '../utils/firebase';
+
+type Product = {
+  id: string;
+  name: string;
+  price: number;
+};
+
+const ProductList = () => {
+  const [products, setProducts] = useState<Product[]>([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const querySnapshot = await getDocs(collection(db, 'products'));
+      const productsArray = querySnapshot.docs.map(doc => ({
+        id: doc.id,
+        ...doc.data()
+      } as Product));
+      setProducts(productsArray);
+    };
+
+    fetchProducts();
+  }, []);
+
+  return (
+    <div>
+      <h1>Product List</h1>
+      <ul>
+        {products.map(product => (
+          <li key={product.id}>{product.name} - ${product.price}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default ProductList;
