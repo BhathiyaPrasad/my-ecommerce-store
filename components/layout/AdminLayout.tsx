ef0484475c5fbae99ef822230d406e06f7954cbb
Bhathiya Prasad <90783913+BhathiyaPrasad@users.noreply.github.com>
2024-07-15 13:23:40 +0530

    setup env variable

---
+++ components/layout/AdminLayout.tsx
@@ -0,0 +1,20 @@
+
+import React, { ReactNode } from 'react';
+import Header from '../common/Header';
+import Footer from '../common/Footer';
+
+type AdminLayoutProps = {
+  children: ReactNode;
+};
+
+const AdminLayout = ({ children }: AdminLayoutProps) => {
+  return (
+    <div>
+      <Header />
+      <main>{children}</main>
+      <Footer />
+    </div>
+  );
+};
+
+export default AdminLayout;
--- utils/firebase.ts
+++ utils/firebase.ts
@@ -2,13 +2,13 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDLa_nr_0c0kudQSzcGV5hkwq3WH2bRGgo",
-  authDomain: "freidea-pos.firebaseapp.com",
-  projectId: "freidea-pos",
-  storageBucket: "freidea-pos.appspot.com",
-  messagingSenderId: "317401437770",
-  appId: "1:317401437770:web:2657a877ea9fe88cf878b3",
-  measurementId: "G-M8RF0RL2FP"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
