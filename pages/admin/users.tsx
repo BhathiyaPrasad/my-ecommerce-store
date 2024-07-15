ff9185f8a7ee4cb3112ecda49aeb96634993874b
Bhathiya Prasad <90783913+BhathiyaPrasad@users.noreply.github.com>
2024-07-15 13:48:16 +0530

    added users view page

---
+++ pages/admin/users.tsx
@@ -0,0 +1,15 @@
+import AdminLayout from '../../components/layout/AdminLayout';
+import AuthGuard from '../../components/AuthGuard';
+import UserManagement from '../../components/admin/UserManagement';
+
+const UserManagementPage = () => {
+  return (
+    <AuthGuard>
+      <AdminLayout>
+        <UserManagement />
+      </AdminLayout>
+    </AuthGuard>
+  );
+};
+
+export default UserManagementPage;
