4dec48573ca946cc31d301c0f4de3006e9643563
Bhathiya Prasad <90783913+BhathiyaPrasad@users.noreply.github.com>
2024-07-15 15:50:24 +0530

    update projects for developing

---
+++ pages/admin/home.tsx
@@ -0,0 +1,12 @@
+import MainLayout from '../../components/layout/AdminLayout';
+import ProductList from '../../components/ProductList';
+
+
+export default function Home() {
+  return (
+    <MainLayout>
+      <ProductList />
+    </MainLayout>
+  );
+}
+
