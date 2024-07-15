ebfc93e11f689dad656dc5b3dfa6bbcca6b21c5a
Bhathiya Prasad <90783913+BhathiyaPrasad@users.noreply.github.com>
2024-07-15 13:47:39 +0530

    index tsx added

---
+++ pages/index.tsx
@@ -0,0 +1,10 @@
+import MainLayout from '../components/layout/MainLayout';
+import ProductList from '../components/ProductList';
+
+export default function Home() {
+  return (
+    <MainLayout>
+      <ProductList />
+    </MainLayout>
+  );
+}
