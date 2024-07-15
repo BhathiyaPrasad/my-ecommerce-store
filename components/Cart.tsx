32ed2a7dd6193b79af66950bd684dbfb8dab6349
Bhathiya Prasad <90783913+BhathiyaPrasad@users.noreply.github.com>
2024-07-15 13:45:33 +0530

    Added cart starting template

---
+++ components/Cart.tsx
@@ -0,0 +1,11 @@
+// components/Cart.tsx
+import React from 'react';
+
+const Cart = () => (
+  <div>
+    <h1>Shopping Cart</h1>
+    {/* Add cart content here */}
+  </div>
+);
+
+export default Cart;
