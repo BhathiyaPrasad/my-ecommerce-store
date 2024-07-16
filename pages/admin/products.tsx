1c7156383ccfaac9800c494c3857e8bd504ce5fd
Bhathiya Prasad <90783913+BhathiyaPrasad@users.noreply.github.com>
2024-07-16 14:20:18 +0530

    import global css

--- pages/admin/products.tsx
+++ pages/admin/products.tsx
@@ -1,7 +1,7 @@
 import AdminLayout from '../../components/layout/AdminLayout';
 // import AuthGuard from '../../components/AuthGuard';
 import ProductManagement from '../../components/admin/ProductManagement';
-
+import '../../src/app/globals.css';
 const ProductManagementPage = () => {
   return (
     // <AuthGuard>
--- pages/admin/users.tsx
+++ pages/admin/users.tsx
@@ -1,7 +1,7 @@
 import AdminLayout from '../../components/layout/AdminLayout';
 // import AuthGuard from '../../components/AuthGuard';
 import UserManagement from '../../components/admin/UserManagement';
-
+import '../../src/app/globals.css';
 const UserManagementPage = () => {
   return (
     // <AuthGuard>
