dca429eaaec50e050f9cfb349edea0cc900aee97
Bhathiya Prasad <90783913+BhathiyaPrasad@users.noreply.github.com>
2024-07-16 17:04:11 +0530

    rename for avoid naming conventions

--- pages/admin/home.tsx
+++ pages/admin/home.tsx
@@ -1,13 +1,13 @@
-import MainLayout from '../../components/layout/AdminLayout';
+import AdminLayout from '../../components/layout/AdminLayout';
 import ProductList from '../../components/ProductList';
 import '../../src/app/globals.css';
 
 export default function Home() {
   return (
-    <MainLayout>
+    <AdminLayout>
       <ProductList />
       
-    </MainLayout>
+    </AdminLayout>
     
   );
 }
