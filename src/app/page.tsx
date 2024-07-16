52b16e683365e36f41a201c68690b5a05ebcded4
Bhathiya Prasad <90783913+BhathiyaPrasad@users.noreply.github.com>
2024-07-16 17:27:03 +0530

    testing ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,13 @@ import Hero from "@components/Hero";
 
 export default function Home() {
   return (
-    <MainLayout><Hero/><h1>THIS IS HOME</h1></MainLayout>
+    <MainLayout>
+      
+      <Hero/>
+    
+    
+    
+    </MainLayout>
   
   );
 }
