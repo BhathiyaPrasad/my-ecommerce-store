036ed061e6e77c6df1cacd4c80b8455dcf7ae00b
Bhathiya Prasad <90783913+BhathiyaPrasad@users.noreply.github.com>
2024-07-16 17:22:55 +0530

    import the component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
 import Image from "next/image";
 import MainLayout from "@components/layout/MainLayout";
-
+import Hero from "@components/Hero";
 
 export default function Home() {
   return (
-    <MainLayout><h1>THIS IS HOME</h1></MainLayout>
+    <MainLayout><Hero/><h1>THIS IS HOME</h1></MainLayout>
   
   );
 }
