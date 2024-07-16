24189e4886d9577b1adc4d68ca8792dcb693d1c1
Bhathiya Prasad <90783913+BhathiyaPrasad@users.noreply.github.com>
2024-07-16 16:58:24 +0530

    update

--- components/common/Header.tsx
+++ components/common/Header.tsx
@@ -5,7 +5,7 @@ import Link from 'next/link';
 
 
 const Header = () => (
-<><div className="navbar bg-base-100">
+  <><div className="navbar bg-base-100">
     <div className="navbar-start">
       <div className="dropdown">
         <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
@@ -25,7 +25,7 @@ const Header = () => (
         <ul
           tabIndex={0}
           className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
-          
+
           <li>
             <a>MEN</a>
             <ul className="p-2">
@@ -40,9 +40,9 @@ const Header = () => (
               <li><a>Pants</a></li>
             </ul>
           </li>
-          
+
           <li><a>THIS WEEK</a></li><li>
-          <a>SALE</a></li>
+            <a>SALE</a></li>
 
         </ul>
       </div>
@@ -50,7 +50,7 @@ const Header = () => (
     </div>
     <div className="navbar-center hidden lg:flex">
       <ul className="menu menu-horizontal px-1">
-     
+
         <li>
           <details>
             <summary>MEN</summary>
@@ -61,56 +61,73 @@ const Header = () => (
           </details>
         </li>
         <li> <details>
-            <summary>WOMEN</summary>
-            <ul className="p-2">
-              <li><a>Pants</a></li>
-              <li><a>Skirts</a></li>
-            </ul>
-          </details></li>
+          <summary>WOMEN</summary>
+          <ul className="p-2">
+            <li><a>Pants</a></li>
+            <li><a>Skirts</a></li>
+          </ul>
+        </details></li>
         <li><a>THIS WEEK</a></li><li>
           <a>SALE</a></li>
 
       </ul>
     </div>
     <div className="navbar-end">
-    <div className="flex-none">
-    <div className="dropdown dropdown-end">
-      <div tabIndex={0} role="button" className="btn btn-ghost btn-circle">
-        <div className="indicator">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-5 w-5"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor">
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
-          </svg>
-          <span className="badge badge-sm indicator-item">8</span>
-        </div>
-      </div>
-      <div
-        tabIndex={0}
-        className="card card-compact dropdown-content bg-base-100 z-[1] mt-3 w-52 shadow">
-        <div className="card-body">
-          <span className="text-lg font-bold">8 Items</span>
-          <span className="text-info">Subtotal: $999</span>
-          <div className="card-actions">
-            <button className="btn btn-primary btn-block">View cart</button>
+      <div className="flex-none">
+        <div className="dropdown dropdown-end">
+          <div tabIndex={0} role="button" className="btn btn-ghost btn-circle">
+            <div className="indicator">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor">
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
+              </svg>
+              <span className="badge badge-sm indicator-item">8</span>
+            </div>
+          </div>
+          <div
+            tabIndex={0}
+            className="card card-compact dropdown-content bg-base-100 z-[1] mt-3 w-52 shadow">
+            <div className="card-body">
+              <span className="text-lg font-bold">8 Items</span>
+              <span className="text-info">Subtotal: $999</span>
+              <div className="card-actions">
+                <button className="btn btn-primary btn-block">View cart</button>
+              </div>
+            </div>
           </div>
         </div>
+        <div className="dropdown dropdown-end">
+          <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
+            <div className="w-10 rounded-full">
+              <img alt="Tailwind CSS Navbar component"
+                src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+            </div>
+          </div>
+          <ul
+            tabIndex={0}
+            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
+            <li>
+              <a className="justify-between">
+                Profile
+                <span className="badge">New</span>
+              </a>
+            </li>
+            <li><a>Settings</a></li>
+            <li><a>Logout</a></li>
+          </ul>
+        </div>
       </div>
     </div>
-
-
-</div>
-
-    </div>
   </div>
-  
+
 
   </>
 
