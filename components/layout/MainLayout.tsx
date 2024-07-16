8d6f40c03db70cccb2e8387ddfafdd16412381db
Bhathiya Prasad <90783913+BhathiyaPrasad@users.noreply.github.com>
2024-07-16 14:46:55 +0530

    set an idea to change theme option

--- components/layout/MainLayout.tsx
+++ components/layout/MainLayout.tsx
@@ -9,7 +9,7 @@ type MainLayoutProps = {
 
 const MainLayout = ({ children }: MainLayoutProps) => {
   return (
-    <div>
+    <div data-theme="light">
       <Header />
       <main>{children}</main>
       <Footer />
