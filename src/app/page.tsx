8b1be91bafb0c1ae6518824e986943925ec6b7ce
Bhathiya Prasad <90783913+BhathiyaPrasad@users.noreply.github.com>
2024-07-16 12:43:19 +0530

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,7 @@ import MainLayout from "@components/layout/MainLayout";
 
 export default function Home() {
   return (
-    <MainLayout>
-<button className="btn btn-primary">Primary</button>
-    </MainLayout>
+    <MainLayout><h1>THIS IS HOME</h1></MainLayout>
+  
   );
 }
