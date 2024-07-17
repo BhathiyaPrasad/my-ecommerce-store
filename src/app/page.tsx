040c23c377bcb0eaf8f44be77817e4ad0a919da9
Bhathiya Prasad <90783913+BhathiyaPrasad@users.noreply.github.com>
2024-07-17 17:01:40 +0530

    import card component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,16 @@
 import Image from "next/image";
 import MainLayout from "@components/layout/MainLayout";
 import Hero from "@components/Hero";
+import ProductCard from "@components/common/ProductCard";
+
 
 export default function Home() {
   return (
     <MainLayout>
       
       <Hero/>
-       
+      
+       <ProductCard />
     
      
     
