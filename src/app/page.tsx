00590c1b94e1aade31bd023dc99f80fb19d76bf8
Bhathiya Prasad <90783913+BhathiyaPrasad@users.noreply.github.com>
2024-07-17 17:22:50 +0530

    imported te title component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,17 +2,17 @@ import Image from "next/image";
 import MainLayout from "@components/layout/MainLayout";
 import Hero from "@components/Hero";
 import ProductCard from "@components/common/ProductCard";
-
+import Title from "@components/common/Title";
 
 export default function Home() {
   return (
     <MainLayout>
       
       <Hero/>
-      
+      <Title text="New Arrivals" />
        <ProductCard />
-    
      
+    
     
     </MainLayout>
   
