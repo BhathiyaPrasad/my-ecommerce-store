b0497797d73afd82795e80d2e0e4d01e6fb798a1
Bhathiya Prasad <90783913+BhathiyaPrasad@users.noreply.github.com>
2024-07-17 17:22:35 +0530

    Created and initialized title component

---
+++ components/common/Title.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Title({text}) {
+    return (
+
+    <button className="btn btn-xs sm:btn-sm md:btn-md lg:btn-lg">{text}</button>
+    
+    )
+}
+
+export default Title
