857649f9846ff41084233c9a22dd1a0273c43eb8
Bhathiya Prasad <90783913+BhathiyaPrasad@users.noreply.github.com>
2024-07-18 13:13:36 +0530

    Created the routeand the page for men items

---
+++ pages/product/men.tsx
@@ -0,0 +1,11 @@
+import MainLayout from "@components/layout/MainLayout";
+import React from "react";
+import ProductList from './../../components/ProductList';
+import '../../src/app/globals.css'
+
+
+export default function men () {
+    return (
+     <MainLayout> <ProductList/></MainLayout>
+    )
+}
