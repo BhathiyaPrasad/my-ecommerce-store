45f55812239e577ffd4baed65594f4ef77a6fbf5
Bhathiya Prasad <90783913+BhathiyaPrasad@users.noreply.github.com>
2024-07-18 09:30:03 +0530

    call the components in main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,14 @@ import MainLayout from "@components/layout/MainLayout";
 import Hero from "@components/Hero";
 import ProductCard from "@components/common/ProductCard";
 import Title from "@components/common/Title";
+import Divider from "@components/common/Divider";
 
 export default function Home() {
   return (
     <MainLayout>
       
       <Hero/>
+      
       <Title text="New Arrivals" />
        <ProductCard />
      
