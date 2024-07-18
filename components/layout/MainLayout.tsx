edfb8af95a659140d355f92502f85dfbd7381d2d
Bhathiya Prasad <90783913+BhathiyaPrasad@users.noreply.github.com>
2024-07-18 17:34:31 +0530

    main layout theme changed

--- components/layout/MainLayout.tsx
+++ components/layout/MainLayout.tsx
@@ -9,7 +9,7 @@ type MainLayoutProps = {
 
 const MainLayout = ({ children }: MainLayoutProps) => {
   return (
-    <div data-theme="dracula">
+    <div data-theme="light">
       <Header />
       <main>{children}</main>
       <ProductCard />
