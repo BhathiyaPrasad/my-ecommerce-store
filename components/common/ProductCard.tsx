ecd77ac54d8cc1925ec566b852f39f3c2e60651f
Bhathiya Prasad <90783913+BhathiyaPrasad@users.noreply.github.com>
2024-07-18 17:14:07 +0530

    used the previously passed props from prodductlist component

--- components/common/ProductCard.tsx
+++ components/common/ProductCard.tsx
@@ -1,27 +1,30 @@
 import React from "react";
 import product from '../../assests/images/COVER WEB.jpg'
 import Image from "next/image";
-import AdminLayout from './../layout/AdminLayout';
-function ProductCard() {
+
+
+
+function ProductCard({Sales_Price, Eng_Name}) {
     return (
-        <div className="card bg-base-10 w-96 shadow-xl">
-  <figure>
-    <Image
-      src={product} priority={false} loading="lazy"
-      alt="Shoes" />
-  </figure>
-  <div className="card-body">
-    <h2 className="card-title">
-      Shoes!
-      <div className="badge badge-secondary">NEW</div>
-    </h2>
-    <p>If a dog chews shoes whose shoes does he choose?</p>
-    <div className="card-actions justify-end">
-      <div className="badge badge-outline">Fashion</div>
-      <div className="badge badge-outline">Products</div>
-    </div>
-  </div>
-</div>
+        <div className="card bg-base-100 w-96 shadow-xl">
+        <figure>
+        <Image
+             src={product}
+                    alt="car!" priority={false} loading="lazy" />
+        </figure>
+        <div className="card-body">
+          <h2 className="card-title">
+          {Eng_Name}
+            <div className="badge badge-secondary">NEW</div>
+          </h2>
+          <p>{Sales_Price}.00</p>
+          <div className="card-actions justify-end">
+            <div className="badge badge-outline">Fashion</div>
+            <div className="badge badge-outline">Products</div>
+          </div>
+        </div>
+      </div>
+        
     )
 
 }
