43dfd0321e1f849680ca412d64e509b29415c793
Bhathiya Prasad <90783913+BhathiyaPrasad@users.noreply.github.com>
2024-07-19 09:35:38 +0530

    call the component and "use client"; for resolve the errors

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,19 +1,17 @@
+"use client";
+
 import React from "react";
 import { collection, getDocs } from 'firebase/firestore';
 import { db } from '../utils/firebase';
 import Image from "next/image";
 import sliderOne from '../assests/images/test.webp';
 import "./Styles/hero.css";
+import ImageSlider from "./ImageSlider";
 
 
 function Hero() {
   return (
-    <div>
-      <figure className="herofigure">
-        <Image src={sliderOne} alt="" className="slider" loading="lazy" priority={false}/>
-      </figure>
-    </div>
-
+   <ImageSlider/>
   )
 }
 
