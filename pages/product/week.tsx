149863c68124325ce1f379673148edddf90548e5
Bhathiya Prasad <90783913+BhathiyaPrasad@users.noreply.github.com>
2024-07-29 15:20:58 +0530

    Created weekly item page

---
+++ pages/product/week.tsx
@@ -0,0 +1,18 @@
+import MainLayout from "@components/layout/MainLayout";
+import React from "react";
+import ProductList from '../../components/ProductList';
+import '../../src/app/globals.css'
+import Title from "@components/common/Title";
+
+
+export default function week () {
+    return (
+     <MainLayout> 
+       <Title text="WEEKLY SPECIALS" />
+     <ProductList
+     category=""
+     order=">"
+     limit=''
+     /></MainLayout>
+    )
+}
