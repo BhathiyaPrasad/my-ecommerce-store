3c84ea6b7108ce437dc25b60814be7e21f85f67a
Bhathiya Prasad <90783913+BhathiyaPrasad@users.noreply.github.com>
2024-08-06 10:04:55 +0530

    integrate clerk as usermanagement thirdparty resource

--- pages/Home.tsx
+++ pages/Home.tsx
@@ -2,18 +2,32 @@ import MainLayout from '../components/layout/MainLayout';
 import ProductList from '../components/ProductList';
 import '../src/app/globals.css'
 import Hero from '@components/Hero';
+import { SignInButton } from '@clerk/nextjs';
+import { SignedIn } from '@clerk/nextjs';
+import { SignedOut } from '@clerk/nextjs';
+import { UserButton } from '@clerk/nextjs';
+import { ClerkProvider } from '@clerk/nextjs';
+
 export default function Home() {
   return (
+    <ClerkProvider>
+    <SignedOut>
+      <SignInButton />
+    </SignedOut>
+    <SignedIn>
+      <UserButton />
+    </SignedIn>
     <MainLayout>
-  <Hero />
+      <Hero />
       <ProductList
-      category=""
-      order=">="
-      limit={12}
-      group='Brand'
-      type=''
-       />
+        category=""
+        order=">="
+        limit={12}
+        group='Brand'
+        type=''
+      />
     </MainLayout>
+    </ClerkProvider>
   );
 }
 
