dd493c21572988e4bfb4f78a06f7219bdad4b7fc
Bhathiya Prasad <90783913+BhathiyaPrasad@users.noreply.github.com>
2024-08-06 17:38:49 +0530

    add middleware file

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,22 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 
-export default clerkMiddleware();
+const isProtectedRoute = createRouteMatcher([
+  '/(.*)',
+]);
+
+export default clerkMiddleware((auth, req) => {
+  const { has, redirectToSignIn } = auth()
+  // Restrict admin routes to users with specific permissions
+  if (isProtectedRoute(req) &&
+    !has({ permission: 'org:sys_memberships:manage' }) ||
+    !has({ permission: 'org:sys_domains_manage' })
+  ){
+
+    // Add logic to run if the user does not have the required permissions
+
+    return redirectToSignIn();
+  }
+});
 
 export const config = {
   matcher: [
