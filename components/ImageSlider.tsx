063cd879d72a1e0c5ac31c5a15086cd13ce7e054
Bhathiya Prasad <90783913+BhathiyaPrasad@users.noreply.github.com>
2024-08-09 14:28:27 +0530

    TEST

--- components/ImageSlider.tsx
+++ components/ImageSlider.tsx
@@ -2,10 +2,10 @@ import React from "react";
 import Slider from "react-slick";
 import '../src/app/globals.css'
 import Image from "next/image";
-import sliderOne from '../assests/images/Slider01.jpg';
-import sliderTwo from '../assests/images/Slider02.jpg';
-import sliderThree from '../assests/images/Slider03.jpg';
-import sliderFour from '../assests/images/Slider04.jpg';
+import sliderOne from '../assests/images/SALUNI COVER 01 (2).webp';
+import sliderTwo from '../assests/images/SALUNI COVER 01 (2).webp';
+import sliderThree from '../assests/images/SALUNI COVER 01 (2).webp';
+import sliderFour from '../assests/images/SALUNI COVER 01 (2).webp';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
