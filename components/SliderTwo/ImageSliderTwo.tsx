fc907fb094e0b5d52b35991db475288d36ca6700
Bhathiya Prasad <90783913+BhathiyaPrasad@users.noreply.github.com>
2024-08-09 17:04:22 +0530

    update

--- components/SliderTwo/ImageSliderTwo.tsx
+++ components/SliderTwo/ImageSliderTwo.tsx
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react'
 import useEmblaCarousel from 'embla-carousel-react'
 import Image from 'next/image'  // Correct import for Next.js Image component
 // import './imageslider.css'
-import sliderOne from '../../assests/images/test.webp';
+import sliderOne from '../../assests/images/Slider01.jpg';
 import sliderTwo from '../../assests/images/Slider02.jpg';
 import sliderThree from '../../assests/images/Slider03.jpg';
 import sliderFour from '../../assests/images/Slider04.jpg';
