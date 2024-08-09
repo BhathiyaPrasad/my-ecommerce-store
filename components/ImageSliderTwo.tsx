64bd6b59a4c8797b42c1febc838d3896f7ac7392
Bhathiya Prasad <90783913+BhathiyaPrasad@users.noreply.github.com>
2024-08-09 13:05:21 +0530

    intizlize embla carousel

--- components/ImageSliderTwo.tsx
+++ components/ImageSliderTwo.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import useEmblaCarousel from 'embla-carousel-react'
 import Image from 'next/image'  // Correct import for Next.js Image component
 import './Styles/imageslider.css'
@@ -6,9 +6,10 @@ import sliderOne from '../assests/images/Slider01.jpg';
 import sliderTwo from '../assests/images/Slider02.jpg';
 import sliderThree from '../assests/images/Slider03.jpg';
 import sliderFour from '../assests/images/Slider04.jpg';
+import Autoplay from 'embla-carousel-autoplay'
 
 export default function ImageSliderTwo() {
-  const [emblaRef] = useEmblaCarousel()
+const [emblaRef] = useEmblaCarousel({ loop: false }, [Autoplay()])
 
   return (
     <div className="embla" ref={emblaRef}>
